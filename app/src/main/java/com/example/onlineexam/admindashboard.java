e06ce47e2004f257936eb12b3003be972d2810c1
savanpethani <savanpethani6@gmail.com>
2024-01-05 14:14:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/onlineexam/admindashboard.java
@@ -0,0 +1,58 @@
+package com.example.onlineexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+
+public class admindashboard extends AppCompatActivity {
+    ImageButton notices,stdmoniter,manageuser,examcontrol;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_admindashboard);
+
+        notices=findViewById(R.id.imageButton6);
+        stdmoniter=findViewById(R.id.imageButton4);
+        manageuser=findViewById(R.id.imageButton5);
+        examcontrol=findViewById(R.id.imageButton3);
+
+        notices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), notices_admin.class);
+                startActivity(intent);
+            }
+        });
+
+        examcontrol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), examschedule.class);
+                startActivity(intent);
+            }
+        });
+
+        stdmoniter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), Studentmonitoring.class);
+                startActivity(intent);
+            }
+        });
+
+        manageuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), ManageUser.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/onlineexam/examselect.java
@@ -0,0 +1,19 @@
+package com.example.onlineexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class examselect extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_examselect);
+
+
+    }
+}
